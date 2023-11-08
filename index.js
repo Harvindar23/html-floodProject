03b9ddd18f030d86b4ae2e10ff9608d09bcf485e
Harvindar23 <134523367+Harvindar23@users.noreply.github.com>
2023-11-08 10:51:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("keydown" , function(event){
+  if ((event.key) ==="Enter"){
+    alert('Thanks for your Feedback');
+}
+  }
+);
+document.querySelector(".sum").addEventListener("click" , function(){
+  alert('Thanks for your Feedback');
+  }
+);
